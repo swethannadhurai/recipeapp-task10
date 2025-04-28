b5ce8739840542e8c482783d5efa3425b77a420b
Swetha <swethaannadhurai@gmail.com>
2025-04-28 12:20:18 +0530

    Fix port binding for Render

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-app.listen('5000','127.0.0.1',()=>{
-  console.log('Server is running at http://127.0.0.1:5000');
-  console.log('press ctrl+c to stop the server');
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
